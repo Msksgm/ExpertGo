f24d8d7fde37ff1b5f460b8f9b207d22eb64b2e4
Msksgm <msksgm3124@gmail.com>
2022-03-20 18:44:17 +0900

    add: section 5

---
+++ section5/list14/list14.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type T struct {
+	A int
+	B string
+	C []int
+	D int
+}
+
+var v T
+
+func main() {
+	fmt.Printf("v address = %p\n", &v)
+	fmt.Printf("v.A address = %p\n", &v.A)
+	fmt.Printf("v.B address = %p\n", &v.B)
+	fmt.Printf("v.C address = %p\n", &v.C)
+
+	fmt.Printf("A size = %d\n", uintptr(unsafe.Pointer(&v.B))-uintptr(unsafe.Pointer(&v.A)))
+	fmt.Printf("B size = %d\n", uintptr(unsafe.Pointer(&v.C))-uintptr(unsafe.Pointer(&v.B)))
+	fmt.Printf("C size = %d\n", uintptr(unsafe.Pointer(&v.D))-uintptr(unsafe.Pointer(&v.C)))
+}
